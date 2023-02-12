efd64c992abbc15578c6966e0d76956562ca9443
황상윤 <s_infinite@naver.com>
2023-02-12 20:54:10 +0900

    섹션 1. 타입스크립트 소개와 배경

--- why-ts/app.js
+++ why-ts/app.js
@@ -1,21 +1,50 @@
 // api url
-var url = 'https://jsonplaceholder.typicode.com/users/1';
+var url = "https://jsonplaceholder.typicode.com/users/1";
 
 // dom
-var username = document.querySelector('#username');
-var email = document.querySelector('#email');
-var address = document.querySelector('#address');
+var username = document.querySelector("#username");
+var email = document.querySelector("#email");
+var address = document.querySelector("#address");
 
 // user data
 var user = {};
 
+/**
+ * @typedef {object} Address
+ * @property {string} street
+ * @property {string} city
+ */
+
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {Address} address
+ */
+
+/**
+ * @returns {Promise<User>}
+ */
+function fetchUser() {
+  return axios.get(url);
+}
+
+fetchUser().then(function (response) {
+  response;
+});
+
 function startApp() {
-  axios
-    .get(url)
+  // axios
+  //   .get(url)
+  fetchUser()
     .then(function (response) {
       console.log(response);
       user = response.data;
+      console.log(user);
       // TODO: 이름, 이메일, 주소 표시하기
+      username.textContent = user?.name;
+      email.textContent = user?.email;
+      address.textContent = user.address?.city;
     })
     .catch(function (error) {
       console.log(error);
---
+++ why-ts/sample.js
@@ -0,0 +1,19 @@
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// var result1 = sum(10, 20);
+// result1.toLocaleString();
+
+// @ts-check
+
+/**
+ * @param {number} a 첫번째 숫자
+ * @param {number} b 두번째 숫자
+ * @returns {number}
+ */
+function sum(a, b) {
+  return a + b;
+}
+
+sum(10, "10");
