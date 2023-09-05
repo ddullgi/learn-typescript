2deb5374c30dc5b61353452fd93f9a401ef519e7
황상윤 <s_infinite@naver.com>
2023-09-05 16:51:02 +0900

    feat. 기본타입 공부

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,22 @@
+// JS 문자열 선언
+// var str = "hello";
+
+// TS 문자열 선언
+const str: string = "hello";
+const num: number = 10;
+const show: boolean = true;
+
+// TS 배열 선언
+const arr: Array<number> = [1, 2, 3];
+const heroes: Array<string> = ["Capt", "Thor", "Hulk"];
+const items: number[] = [1, 2, 3];
+
+// TS 튜플
+let adress: [string, number] = ["gangnam", 100];
+
+// TS 객체
+const obj: object = {};
+const person: { name: string; age: number } = {
+  name: "thor",
+  age: 1000,
+};
